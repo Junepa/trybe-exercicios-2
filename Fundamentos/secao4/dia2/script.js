39cd7cf686109ae45377baaa0479d32110326e8b
Juliana Neves <juliananeves@pop-os.localdomain>
2022-11-25 18:48:17 -0300

    exercicios continuação

--- Fundamentos/secao4/dia2/script.js
+++ Fundamentos/secao4/dia2/script.js
@@ -13,6 +13,16 @@
 //let indexOfPortfolio = menu.idexOf('Portfólio');
 //console.log(indexOfPortfolio);
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push ('contato')
-console.log(menu);
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//menu.push ('contato')
+//console.log(menu);
+
+//console.log(cars[0]); // Saab
+//console.log(cars[1]); // Volvo
+//console.log(cars[2]); // BMW
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+for (let index = 0; index < cars.length; index += 1);{
+
+    console.log(cars[index]);
+}
