6c1e9c79ce1caecc466b32e22201d0e6da341df1
Juliana Neves <juliananeves@pop-os.localdomain>
2022-11-25 18:24:49 -0300

    exercicios dia 2

---
+++ Fundamentos/secao4/dia2/script.js
@@ -0,0 +1,18 @@
+
+//let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+//let searchForFirstTask = tasksList[0];
+//console.log(searchForFirstTask);
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu[1];
+
+//console.log(menuServices);
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let indexOfPortfolio = menu.idexOf('Portfólio');
+//console.log(indexOfPortfolio);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push ('contato')
+console.log(menu);
