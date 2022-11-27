531d8c89d32e92394139d49264be732f678ceaca
Juliana Neves <juliananeves@pop-os.localdomain>
2022-11-26 22:08:03 -0300

    encontrando o meno valor do array

--- Fundamentos/secao4/dia2/script.js
+++ Fundamentos/secao4/dia2/script.js
@@ -76,12 +76,22 @@ let media = soma / numbers.length;
 
 //Exercício -6
 //Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let resultado = 0
+//for (let index=0; index < numbers.length; index += 1){
+    //if numbers[index] % 2 !== 0 {
+        //resultado +=1
+    // }
+//} if (numbers[index] % == 0){
+    //console.log (nenhum valor ímpar encontrado)
+//}
+
+// Exercício -7
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0
-for (let index=0; index < numbers.length; index += 1){
-    if numbers[index] % 2 !== 0 {
-        resultado +=1
-     }
-} if (numbers[index] % == 0){
-    console.log (nenhum valor ímpar encontrado)
-}
+let menorNumero = numbers [0];
+for (let index =1; index < numbers.length; index += 1){
+    if(number[index] < menorNumero) {
+        menorNumero = number [index]
+    }
+} console.log(menorNumero)
