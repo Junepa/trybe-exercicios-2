c536868be452347f29660045c60477f256622230
Juliana Neves <juliananeves@pop-os.localdomain>
2022-11-26 21:09:13 -0300

    desenvolvendo exercicios JS

--- Fundamentos/secao4/dia2/script.js
+++ Fundamentos/secao4/dia2/script.js
@@ -26,13 +26,32 @@
 
     //console.log(cars[index]);
 
+    //exercicio 1
     //let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     //for(let index = 0; index < numbers.lenght; index += 1){
    //     console.log(numbers[index]);
   // }
-  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index += 1) {
+//for (let index = 0; index < numbers.length; index += 1) {
   
-    console.log(numbers[index]);
+    // console.log(numbers[index]);
+//}
+
+//exercicio - 2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0;
+
+//for (let index = 0; index < numbers.length; index +=1) {
+//    soma += numbers[index]; }
+/
+ //   console.log(soma)
+
+
+//exercicio-3
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0
+for (let index = 0; index < numbers.length; index += 1); {
+    soma += numbers[index];
 }
+let media = soma / numbers.length
