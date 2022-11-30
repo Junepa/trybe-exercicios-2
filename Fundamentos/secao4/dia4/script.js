ef5e5c1dcc22cf78cd91d2836c1a977e5de3e0f3
Juliana Neves <juliananeves@pop-os.localdomain>
2022-11-30 15:47:42 -0300

    exemplos/objetos

---
+++ Fundamentos/secao4/dia4/script.js
@@ -0,0 +1,20 @@
+// let diasDaSemana = {
+//     1: 'domingo',
+//     2: 'segunda',
+//     3: 'terça',
+//     4: 'quarta',
+//     5: 'quinta',
+//     6: 'sexta',
+//     7: 'sábado',
+//   };
+//   console.log(diasDaSemana['1']); 
+let conta = {
+    agencia: '0000',
+    banco: {
+      cod: '012',
+      id: 4,
+      nome: 'TrybeBank',
+    },
+  };
+  let infoDoBanco = 'banco';
+console.log(conta[infoDoBanco]);
