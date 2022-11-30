99438200a8c234afbf8f40f669cdedeb96814564
Juliana Neves <juliananeves@pop-os.localdomain>
2022-11-30 18:36:39 -0300

    exerciciosobjetos

--- Fundamentos/secao4/dia4/script.js
+++ Fundamentos/secao4/dia4/script.js
@@ -1,3 +1,4 @@
+// Exemplo 1
 // let diasDaSemana = {
 //     1: 'domingo',
 //     2: 'segunda',
@@ -7,14 +8,29 @@
 //     6: 'sexta',
 //     7: 'sábado',
 //   };
-//   console.log(diasDaSemana['1']); 
-let conta = {
-    agencia: '0000',
-    banco: {
-      cod: '012',
-      id: 4,
-      nome: 'TrybeBank',
-    },
-  };
-  let infoDoBanco = 'banco';
-console.log(conta[infoDoBanco]);
+//   console.log(diasDaSemana['1']);
+
+//Exemplo 2
+// let conta = {
+//     agencia: '0000',
+//     banco: {
+//       cod: '012',
+//       id: 4,
+//       nome: 'TrybeBank',
+//     },
+//   };
+//   let infoDoBanco = 'banco';
+// console.log(conta[infoDoBanco]);
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+
+   };
+  // console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+
+  player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]; 
+  console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
