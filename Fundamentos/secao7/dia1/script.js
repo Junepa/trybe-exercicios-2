30eaef9aed4165c2eb42e7a468d0b8e826d56c21
Juliana Neves <juliananeves@pop-os.localdomain>
2023-01-19 20:54:10 -0300

    verifica números

--- Fundamentos/secao7/dia1/script.js
+++ Fundamentos/secao7/dia1/script.js
@@ -12,3 +12,25 @@ const sum = (value1, value2) => {
   }
 }
 console.log(sum(5, ));
+
+const verifyIsNumber = (value1, value2) => {
+  if (!value1 || !value2) { // A condição do if só será satisfeita se ao menos um dos campos não tenha sido preenchido
+    throw new Error('Preencha os campos para realizar a soma'); // Lançando erro descritivo
+  }
+}
+
+const verificaSoma = (value1, value2) => {
+  if(!value1 || !value2){
+    throw new Error('Preencha os dois campos')
+  }
+}
+
+// Verifique se os valores inseridos são numéricos:
+
+const verificaSoma = (value1, value2) => {
+  if(typeof value1 !== 'number' || typeof value2 !== 'number'){
+    return ('Favor inserir números no campo')
+  }
+}
+console.log(verificaSoma(2, '2'))
+
