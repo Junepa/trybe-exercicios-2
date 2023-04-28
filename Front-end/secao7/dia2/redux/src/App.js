cbc3211d02c35cc5c4d269f2d8c78fd3f8fd8521
Juliana <junepa@gmail.com>
2023-04-28 12:01:06 -0300

    exercicios FrontEnd

---
+++ Front-end/secao7/dia2/redux/src/App.js
@@ -0,0 +1,18 @@
+// ./src/App.js
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h2>0</h2>
+        <button>Incrementa 1</button>
+        <button>Incrementa 5</button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
